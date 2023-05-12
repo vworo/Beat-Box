c0b5dde6dfa06515fde648f347b1fbcebd2ff344
vworo <victorworobec@gmail.com>
2023-05-12 11:05:54 +1000

    Proper routing to Spotify and redirect back to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const client_id = 'db3fb3b60f7c44cf843733eb2c0976bf';
 const redirect_uri = 'http://localhost:5173';
+
 let SERVER_URL = 'https://accounts.spotify.com/authorize';
 SERVER_URL += '?response_type=token';
 SERVER_URL += '&client_id=' + encodeURIComponent(client_id);
@@ -11,15 +12,25 @@ SERVER_URL += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 function App() {
 
   const authorize = () => {
-    axios(SERVER_URL).then((response) => {
-        console.log(response);
-    });
+        window.location.href = SERVER_URL;
   };
+
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    const token = hash.split('&')[0].split('=')[1];
+    if (token) {
+      setAccessToken(token);
+    }
+  }, []);
+
+  
 
   return (
     <div>
       <input type="text" />
-      <button onClick={ authorize }>Get Song</button>
+      <button onClick={ authorize }>Log in on Spotify</button>
 
       <BrowserRouter>
         <Routes>
