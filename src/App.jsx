ae63a95465a25c056c611f01f9c48bf75ffd7d71
Yosiyas7 <josiahdebessay@gmail.com>
2023-05-12 11:33:14 +1000

    removed login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const client_id = 'db3fb3b60f7c44cf843733eb2c0976bf';
 const redirect_uri = 'http://localhost:5173';
-
 let SERVER_URL = 'https://accounts.spotify.com/authorize';
 SERVER_URL += '?response_type=token';
 SERVER_URL += '&client_id=' + encodeURIComponent(client_id);
@@ -11,13 +11,15 @@ SERVER_URL += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 function App() {
 
   const authorize = () => {
-        window.location.href = SERVER_URL;
+    axios(SERVER_URL).then((response) => {
+        console.log(response);
+    });
   };
 
   return (
     <div>
       <input type="text" />
-      <button onClick={ authorize }>Log in on Spotify</button>
+      <button onClick={ authorize }>Get Song</button>
 
       <BrowserRouter>
         <Routes>
