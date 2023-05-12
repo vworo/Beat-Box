b33a49630c34f0e3ff2ec6fb3a96617623f34681
vworo <victorworobec@gmail.com>
2023-05-12 16:23:28 +1000

    Correctly changes usestate for token"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from 'react'
 
+// Spotify OAuth URLs/redirects
 const client_id = 'db3fb3b60f7c44cf843733eb2c0976bf';
+const client_secret = 'cdfb228fe09841ef863840d271a8d739';
 const redirect_uri = 'http://localhost:5173';
 let server_url = 'https://accounts.spotify.com/authorize';
 server_url += '?response_type=token';
@@ -11,27 +12,20 @@ server_url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 
 function App() {
 
-  const [accessToken, setAccessToken] = useState('');
+  // Create usestate to keep track of token across all components within App
+  const [accessToken, setAccessToken] = useState(null);
 
-  useEffect(() => {
-    const hash = window.location.hash;
-    const token = hash.split('&')[0].split('=')[1];
-    if (token) {
-      setAccessToken(token);
-    }
-  }, []);
-
+  // Changes current URL the server_url where user will login to Spotify
   const authorize = () => {
     window.location.href = server_url;
   };
 
+  // Call api to get token
   const showToken = () => {
-    const client_id = 'db3fb3b60f7c44cf843733eb2c0976bf';
-    const client_secret = 'cdfb228fe09841ef863840d271a8d739';
 
+    // Create URL for axious with the relevant headers and client data
     const url = 'https://accounts.spotify.com/api/token';
     const data = 'grant_type=client_credentials';
-
     const config = {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -39,25 +33,25 @@ function App() {
       }
     };
 
+    // Make request to server with above data
     axios.post(url, data, config)
     .then((response) => {
-      console.log(response.data.access_token);
+      setAccessToken(response.data.access_token);
+      console.log('Access token updated:', accessToken);
     })
     .catch((error) => {
       console.log(error);
     });
   }
 
+  useEffect(() => {
+    console.log('Access token changed:', accessToken);
+  }, [accessToken]);
+
   return (
     <div>
       <button onClick={ authorize }>Login</button>
       <button onClick={ showToken }>Show Token</button>
-
-      {/* <BrowserRouter>
-        <Routes>
-          <Route  />
-        </Routes>
-      </BrowserRouter> */}
     </div>
   )
 }
