b8bafc239841cd969378849471a2bae8ff2aaf2f
vworo <victorworobec@gmail.com>
2023-05-12 14:41:42 +1000

    Some progress towards retreiving token

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,57 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
+import { useState, useEffect } from 'react'
 
 const client_id = 'db3fb3b60f7c44cf843733eb2c0976bf';
 const redirect_uri = 'http://localhost:5173';
-let SERVER_URL = 'https://accounts.spotify.com/authorize';
-SERVER_URL += '?response_type=token';
-SERVER_URL += '&client_id=' + encodeURIComponent(client_id);
-SERVER_URL += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+let server_url = 'https://accounts.spotify.com/authorize';
+server_url += '?response_type=token';
+server_url += '&client_id=' + encodeURIComponent(client_id);
+server_url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 
 function App() {
 
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    const token = hash.split('&')[0].split('=')[1];
+    if (token) {
+      setAccessToken(token);
+    }
+  }, []);
+
   const authorize = () => {
-    window.location.href = SERVER_URL;
+    window.location.href = server_url;
   };
+
+  const showToken = () => {
+    const client_id = 'CLIENT_ID';
+    const client_secret = 'CLIENT_SECRET';
+
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+      },
+      form: {
+        grant_type: 'client_credentials'
+      },
+      json: true
+    };
+
+    // axios("https://accounts.spotify.com/api/token").then((response) => {
+    //console.log(response);
+    axios(authOptions).then((error, response, body) => {
+        console.log(response);
+        console.log(body);
+    }) 
+  }
 
   return (
     <div>
       <button onClick={ authorize }>Login</button>
+      <button onClick={ showToken }>Show Token</button>
 
       {/* <BrowserRouter>
         <Routes>
@@ -30,3 +65,4 @@ function App() {
 export default App
 
 // client id : db3fb3b60f7c44cf843733eb2c0976bf
+// client secret: cdfb228fe09841ef863840d271a8d739
