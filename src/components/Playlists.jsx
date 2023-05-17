c7988bec4caee29c41d2704d70d4df99a71cad5c
trstnb1998 <tristanbartolome@hotmail.com>
2023-05-17 21:50:00 +1000

    artists added

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -2,39 +2,43 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
-export default function Playlists(props) {
+export default function Playlists() {
   const [displaySongs, setDisplaySongs] = useState([]);
   const { state } = useLocation();
 
   useEffect(() => {
-    const loadSongs = (token, playlistId) => {
+    const loadSongs = (token) => {
       axios
-        .get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        .get(`https://api.spotify.com/v1/playlists/${state.displayPlaylist.id}/tracks`, {
           headers: { Authorization: `Bearer ${token}` },
         })
         .then((response) => {
           setDisplaySongs(response.data.items);
+        })
+        .catch((error) => {
+          console.log("Error retrieving playlist songs:", error);
         });
     };
 
-    if (state && state.displayPlaylist) {
-      loadSongs(state.token, state.displayPlaylist.id);
+    if (state && state.token && state.displayPlaylist) {
+      loadSongs(state.token);
     }
-  }, [state?.token, state?.displayPlaylist]);
-
-  let playlistSongs =
-    displaySongs &&
-    displaySongs.map((item, index) => (
-      <li key={index}>
-        <span>{item.track.name}</span> - <span>{item.track.artists[0].name}</span>
-      </li>
-    ));
+  }, [state]);
 
   if (state && state.displayPlaylist) {
     return (
       <div>
         <h1>{state.displayPlaylist.name}</h1>
-        <ul>{playlistSongs}</ul>
+        <ul>
+          {displaySongs.map((item, index) => (
+            <li key={index}>
+              <span>{item.track.name}</span> -{" "}
+              {item.track.artists.map((artist, artistIndex) => (
+                <span key={artistIndex}>{artist.name}</span>
+              ))}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   } else {
